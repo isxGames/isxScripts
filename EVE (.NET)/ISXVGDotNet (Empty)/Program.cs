8032722405d0a5de5aeaa7600d1b49632afe6152
GliderPro <gliderpro@gmail.com>
2007-11-11 18:44:10 +0000

    ported ISXVG sample to ISXEVE

---
+++ EVE (.NET)/ISXVGDotNet (Empty)/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EVE.ISXEVE;
+using LavishVMAPI;
+using InnerSpaceAPI;
+using LavishScriptAPI;
+
+namespace ISXEVEDotNet
+{
+    static class Program
+    {
+        static void Main()
+        {
+            // Your code goes here!
+
+            return;
+        }
+    }
+}
