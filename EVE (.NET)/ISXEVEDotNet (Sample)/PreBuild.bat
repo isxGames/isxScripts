c50c930bdcec54d22d79d989c0a6781372dd7a9d
gliderpro <gliderpro@80ca7822-4b20-0410-9c26-f5be05826d28>
2007-11-11 19:09:37 +0000

    edit batch files for ISXEVE deleting unwanted files renamed project files

--- EVE (.NET)/ISXEVEDotNet (Sample)/PreBuild.bat
+++ EVE (.NET)/ISXEVEDotNet (Sample)/PreBuild.bat
@@ -2,5 +2,5 @@
 :: delims is a TAB followed by a space
 FOR /F "tokens=2* delims=	 " %%A IN ('REG QUERY "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\InnerSpace.exe" /v "Path"') DO SET InnerSpacePath=%%B
 
-copy "%InnerSpacePath%\.NET Programs\ISXVGWrapper.dll" "..\.."
+copy "%InnerSpacePath%\.NET Programs\ISXEVEWrapper.dll" "..\.."
 copy "%InnerSpacePath%\Lavish.InnerSpace.dll" "..\.."
