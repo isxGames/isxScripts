656e1bab60828966286dbcb209ee62238634f9bb
GliderPro <gliderpro@gmail.com>
2007-11-11 21:23:48 +0000

    Update source to work with ISXEVE.

--- EVE (.NET)/ISXEVEDotNet (Empty with forms)/Form1.cs
+++ EVE (.NET)/ISXEVEDotNet (Empty with forms)/Form1.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
-namespace ISXVGDotNet
+namespace ISXEVEDotNet
 {
     public partial class Form1 : Form
     {
--- EVE (.NET)/ISXEVEDotNet (Empty with forms)/Properties/AssemblyInfo.cs
+++ EVE (.NET)/ISXEVEDotNet (Empty with forms)/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ISXVGDotNet")]
+[assembly: AssemblyTitle("ISXEVEDotNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ISXVGDotNet")]
+[assembly: AssemblyProduct("ISXEVEDotNet")]
 [assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
