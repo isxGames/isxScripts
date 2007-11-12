220b6cd71caff598c367958e74a4dfb6e8fff6ce
GliderPro <gliderpro@gmail.com>
2007-11-12 20:34:54 +0000

    Converted project to VS2008. Made the example actually do something.

--- EVE (.NET)/ISXEVEDotNet (Sample with forms)/Form1.cs
+++ EVE (.NET)/ISXEVEDotNet (Sample with forms)/Form1.cs
@@ -19,46 +19,56 @@ namespace ISXEVEDotNet
         public Form1()
         {
             InitializeComponent();
-            Extension Ext = new Extension();
-            LavishVMAPI.Frame.Lock();
-            //NameLabel.Text = "The first ability in your AbilitiesArray is: " + Ext.Me.Ability(1).Name;
-            LavishVMAPI.Frame.Unlock();
+            using (new FrameLock(true))
+            {
+                NameLabel.Text = "Your character's name is " + Extension.Me().Name;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Extension Ext = new Extension();
-            LavishVMAPI.Frame.Lock();
-            //NameLabel.Text = "Your Name is: " + Ext.Me.FName + " " + Ext.Me.LName;
-            LavishVMAPI.Frame.Unlock();
+            using (new FrameLock(true))
+            {
+                NameLabel.Text = "Your active ship has " + Extension.Me().Ship().HighSlots + " high slots.";
+            }
             button1.Visible = false;
             button2.Visible = true;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Extension Ext = new Extension();
-            LavishVMAPI.Frame.Lock();
-            //NameLabel.Text = "Your Level is: " + Ext.Me.Level.ToString();
+            using (new FrameLock(true))
+            {
+                NameLabel.Text = "Your active ship has " + Extension.Me().Ship().MediumSlots + " medium slots.";
+            }
             button2.Visible = false;
             button3.Visible = true;
-            LavishVMAPI.Frame.Unlock();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Extension Ext = new Extension();
-            LavishVMAPI.Frame.Lock();
-            //NameLabel.Text = "Your Health is: " + Ext.Me.HealthStr;
+            using (new FrameLock(true))
+            {
+                NameLabel.Text = "Your active ship has " + Extension.Me().Ship().LowSlots + " low slots.";
+            }
             button3.Visible = false;
             button4.Visible = true;
-            LavishVMAPI.Frame.Unlock();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            //VGLocationsForm LocationsForm = new VGLocationsForm();
-            //LocationsForm.ShowDialog();
+            using (new FrameLock(true))
+            {
+                if (Extension.Me().InStation)
+                {
+                    NameLabel.Text = "Undocking...";
+                    Extension.EVE().Execute(ExecuteCommand.CmdExitStation);
+                }
+                else
+                {
+                    NameLabel.Text = "You are in space.";
+                }
+            }
 
             button4.Visible = false;
             button1.Visible = true;
