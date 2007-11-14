82d448892ff295a79e2984677ce1730898721465
GliderPro <gliderpro@gmail.com>
2007-11-14 17:47:55 +0000

    Removed file/folder

--- EVE (.NET)/WPF Sample/bin/Debug/PostBuild.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-:: delims is a TAB followed by a space
-FOR /F "tokens=2* delims=	 " %%A IN ('REG QUERY "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\InnerSpace.exe" /v "Path"') DO SET InnerSpacePath=%%B
-
-
-copy %1 "%InnerSpacePath%\.NET Programs"
