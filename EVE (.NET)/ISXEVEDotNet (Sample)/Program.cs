3e09fbecaecf6a031a0be277464365cc45ccc82d
GliderPro <gliderpro@gmail.com>
2007-11-14 20:31:12 +0000

    working sample

--- EVE (.NET)/ISXEVEDotNet (Sample)/Program.cs
+++ EVE (.NET)/ISXEVEDotNet (Sample)/Program.cs
@@ -8,17 +8,39 @@ using LavishScriptAPI;
 
 namespace ISXEVEDotNet
 {
+    public sealed class Globals
+    {
+        public static readonly Globals Instance = new Globals();
+
+        public EVE.ISXEVE.Extension Ext = new EVE.ISXEVE.Extension();
+
+        private Globals() { }
+    }
+
     static class Program
     {
         static void Main()
         {
-            LavishVMAPI.Frame.Lock();
+            Globals Globals = Globals.Instance;
 
-            Me me = new Me();
-            string MyNameIs = "My Name is " + me.Name;
-            InnerSpace.Echo(MyNameIs);
+            using (new FrameLock(true))
+            {
+                InnerSpace.Echo("Your character's name is " + Globals.Ext.Me().Name);
+                InnerSpace.Echo("Your active ship has " + Globals.Ext.Me().Ship().HighSlots + " high slots.");
+                InnerSpace.Echo("Your active ship has " + Globals.Ext.Me().Ship().MediumSlots + " medium slots.");
+                InnerSpace.Echo("Your active ship has " + Globals.Ext.Me().Ship().LowSlots + " low slots.");
+                if (Globals.Ext.Me().InStation)
+                {
+                    InnerSpace.Echo("Undocking...");
+                    Globals.Ext.EVE().Execute(ExecuteCommand.CmdExitStation);
+                }
+                else
+                {
+                    InnerSpace.Echo("You are in space.");
+                }
 
-            LavishVMAPI.Frame.Unlock();
+            }
+
             return;
         }
     }
