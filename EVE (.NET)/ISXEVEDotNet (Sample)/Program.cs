dc277c9cc3fa1a9c339dae5c3289471be0fe8b61
GliderPro <gliderpro@gmail.com>
2007-11-15 22:40:26 +0000

    Added chat event support

--- EVE (.NET)/ISXEVEDotNet (Sample)/Program.cs
+++ EVE (.NET)/ISXEVEDotNet (Sample)/Program.cs
@@ -22,6 +22,7 @@ namespace ISXEVEDotNet
         static void Main()
         {
             Globals Globals = Globals.Instance;
+            EveEvents evt = new EveEvents();
 
             using (new FrameLock(true))
             {
@@ -31,15 +32,18 @@ namespace ISXEVEDotNet
                 InnerSpace.Echo("Your active ship has " + Globals.Ext.Me().Ship().LowSlots + " low slots.");
                 if (Globals.Ext.Me().InStation)
                 {
-                    InnerSpace.Echo("Undocking...");
-                    Globals.Ext.EVE().Execute(ExecuteCommand.CmdExitStation);
+                    // Uncomment the line below to actually undock
+                    //Globals.Ext.EVE().Execute(ExecuteCommand.CmdExitStation);
                 }
                 else
                 {
                     InnerSpace.Echo("You are in space.");
                 }
-
             }
+
+            InnerSpace.Echo("Pausing for two minutes.");
+            System.Threading.Thread.Sleep(120000);
+            InnerSpace.Echo("Exiting program.");
 
             return;
         }
