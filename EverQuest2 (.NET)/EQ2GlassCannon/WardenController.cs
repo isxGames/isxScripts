cc6da973a52a06d3b46664f1f4265543252735bf
eccentric <eccentric@80ca7822-4b20-0410-9c26-f5be05826d28>
2009-08-10 03:36:35 +0000

    Added stubs for druid and warden, and rudimentary dps for warden.

---
+++ EverQuest2 (.NET)/EQ2GlassCannon/WardenController.cs
@@ -0,0 +1,72 @@
+﻿/* NOTE: The warden implementation is nowhere near complete.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EQ2GlassCannon
+{
+	public class WardenController : DruidController
+	{
+		protected int m_iSingleElementalDebuffAbilityID = -1;
+		protected int m_iSingleHeatNukeAbilityID = -1;
+		protected int m_iSingleColdNukeAbilityID = -1;
+		protected int m_iBlueMeleeColdAEAbilityID = -1;
+
+		public override void RefreshKnowledgeBook()
+		{
+			base.RefreshKnowledgeBook();
+
+			m_iSingleElementalDebuffAbilityID = SelectHighestTieredAbilityID("Frostbite");
+			m_iSingleHeatNukeAbilityID = SelectHighestTieredAbilityID("Dawnstrike");
+			m_iSingleColdNukeAbilityID = SelectHighestTieredAbilityID("Icefall");
+			m_iBlueMeleeColdAEAbilityID = SelectHighestAbilityID("Whirl of Permafrost");
+			return;
+		}
+
+		/************************************************************************************/
+		public bool AttemptCures()
+		{
+			return AttemptCures(true, false, false, true);
+		}
+
+		/************************************************************************************/
+		public override bool DoNextAction()
+		{
+			if (base.DoNextAction())
+				return true;
+
+			if (Me.CastingSpell || MeActor.IsDead)
+				return true;
+
+			if (m_bPrioritizeCures && AttemptCures())
+				return true;
+
+			/// Start this early just to get pet and autoattack rolling (illusionist mythical regen depends on it).
+			/// We don't attempt offensive action until after cures/heals are dealt with.
+			GetOffensiveTargetActor();
+
+			bool bOffensiveTargetEngaged = EngagePrimaryEnemy();
+
+			if (m_OffensiveTargetActor != null)
+			{
+				if (CastBlueOffensiveAbility(m_iBlueMeleeColdAEAbilityID, 4))
+					return true;
+
+				if (MeActor.IsIdle)
+				{
+					if (CastAbility(m_iSingleElementalDebuffAbilityID))
+						return true;
+					if (CastAbility(m_iSingleColdNukeAbilityID))
+						return true;
+					if (CastAbility(m_iSingleHeatNukeAbilityID))
+						return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
