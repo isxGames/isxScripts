07338339425996f6007b4d1f1a4226c0b6d8e6b9
eccentric <eccentric@80ca7822-4b20-0410-9c26-f5be05826d28>
2009-10-18 21:27:12 +0000

    Added a static placeholder class for any Win32 functions I decide to use down the road.

---
+++ EverQuest2 (.NET)/EQ2GlassCannon/EQ2GlassCannon/PInvoke.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace EQ2GlassCannon
+{
+	public static class PInvoke
+	{
+		[DllImport("kernel32.dll")]
+		static extern bool SetProcessAffinityMask(IntPtr hProcess, UIntPtr dwProcessAffinityMask);
+
+		[DllImport("kernel32.dll")]
+		static extern UIntPtr SetThreadAffinityMask(IntPtr hThread, UIntPtr dwThreadAffinityMask);
+	}
+}
